31ff5519445393fa6949068e5e5ebd8ab7e50a57
Marcos Nino <tmnino@uninorte.edu.co>
2022-05-03 17:09:24 -0500

    Reducer
    Three functions were created to be able to add title and message by clicking the button on the webpage

--- src/components/Reducer.jsx
+++ src/components/Reducer.jsx
@@ -4,7 +4,19 @@ const Reducer = (state, action) => {
     switch(action.type) {
         case 'add-note':
             console.log("adding note");
-            return state
+            const newNote = {
+                id : Math.floor(Math.random() * 100),
+                message : action.payload.message,
+                title :action.payload.title,
+                done : false
+            }
+
+            const newListOfNotesAddedOne = [...state.listOfNotes, newNote]
+
+            const newStateAddNote = {
+                ...state, listOfNotes: newListOfNotesAddedOne
+            }
+            return newStateAddNote
         case 'remove-note':
             return state
         case 'update-note':
