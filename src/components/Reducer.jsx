65454b0b7a9183588c750972fec5cf8dde798c79
Marcos Nino <tmnino@uninorte.edu.co>
2022-05-03 14:13:05 -0500

    Reducer
    Reducer component was created with a Reducer function

--- src/components/Reducer.jsx
+++ src/components/Reducer.jsx
@@ -1,1 +1,15 @@
 //This component will help us to manage the global state
+
+const Reducer = (state, action) => {
+    switch(action.type) {
+        case 'add-note':
+            return state
+        case 'remove-note':
+            return state
+        case 'update-note':
+            return state
+        
+    }
+}
+
+export default Reducer;
